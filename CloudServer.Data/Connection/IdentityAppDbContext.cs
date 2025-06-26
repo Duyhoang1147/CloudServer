b9165781c0d5ac876924f947b7dd8cf9e697d734
Duyhoang1147 <115346797+Duyhoang1147@users.noreply.github.com>
2025-06-26 13:48:10 +0700

    Install core Identity and Connect SQL

---
+++ CloudServer.Data/Connection/IdentityAppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CloudServer.Data.Entity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace CloudServer.Data.Connection
+{
+    public class IdentityAppDbContext : IdentityDbContext<ApplicationUser, IdentityRole, string>
+    {
+        public IdentityAppDbContext(DbContextOptions<IdentityAppDbContext> options) : base(options)
+        {
+        }
+    }
+}
